f0dd82c3d11a931182567fc64834e7b06325de9a
mictlan <mictlan@acervo.org>
2010-06-27 15:21:20 -0500

    Agregar icemixer.sh para transmission sin X
      * icemixer.sh abre un screen con ventanitas de alsamixer, darkice y
    ecasignalview

---
+++ config/chroot_local-includes/usr/bin/icemixer.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# icemixer-screen.sh
+# launches darkice, ecasignalview, and alsamixer
+# within screen for a simultaneous console view
+# of mic levels, mixer, and darkice output
+
+# set console font to the smallest size
+setfont /usr/share/consolefonts/lat0-08.psf.gz
+
+# create a custom screenrc just for flujos
+cat > /home/radio/.screenrc-flujos <<-EOF
+	startup_message off
+	screen 0 /usr/bin/ecasignalview -o alsa
+	split
+	resize 12
+	focus
+	screen 1 /usr/bin/darkice
+	split -v
+	focus
+	screen 2 /usr/bin/alsamixer -V capture
+EOF
+
+# dismiss ecasignalview whining
+export ECASOUND=ecasound
+
+# ensure darkice.cfg defaults to alsa
+sudo sed -i '/^device/s/jack/default/' /etc/darkice.cfg
+#sudo sed -i '/^server/s/localhost/$1/' /etc/darkice.cfg
+
+# launch icecast server
+pgrep icecast2 || sudo /etc/init.d/icecast2 start
+
+# launch screen with custom screenrc
+screen -c /home/radio/.screenrc-flujos
+
+# THE END
