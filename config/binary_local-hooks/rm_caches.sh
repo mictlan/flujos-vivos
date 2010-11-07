42f7c8bd47b6c73689e4dda0d577c0118347879c
Mictlan <mictlan@acervo.org>
2010-11-07 11:11:29 -0600

    hook para quitar cache de apt

--- config/binary_local-hooks/rm_caches.sh
+++ config/binary_local-hooks/rm_caches.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-rm -f /var/lib/apt/lists/*_{Packages,Release,Release.gpg}
+#rm -f /var/lib/apt/lists/*_{Packages,Release,Release.gpg}
+rm -f /var/lib/apt/lists/*
+
 rm -f /usr/share/icons/gnome/icon-theme.cache
